1e594689bd5c7334d406ca00f8c578d7b30fde21
MalikaHEDDAJI03 <Malika.Heddaji@emsi-edu.ma>
2025-10-31 00:39:37 +0100

    first commit

---
+++ src/main/java/ma/rest/spring/entities/Compte.java
@@ -0,0 +1,63 @@
+package ma.rest.spring.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Data
+public class Compte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+
+    @Enumerated(EnumType.STRING)
+    private TypeCompte type;
+
+    public Compte(Long id, double solde, Date dateCreation, TypeCompte type) {
+        this.id = id;
+        this.solde = solde;
+        this.dateCreation = dateCreation;
+        this.type = type;
+    }
+
+    public Compte() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public TypeCompte getType() {
+        return type;
+    }
+
+    public void setType(TypeCompte type) {
+        this.type = type;
+    }
+}
